ad5bb76e813e9370f49332551529469f9181ae31
darkenvy <darkenvy6@gmail.com>
2016-10-10 21:53:27 -0700

    small changes

---
+++ process/typescript/main.ts
@@ -0,0 +1,17 @@
+console.log("success");
+
+// Wait for Dom to be ready before listening for file loadings.
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('file').addEventListener('change', loadFile);
+});
+
+function loadFile(evt) {
+  var selFile = evt.target.files[0];
+  var reader = new FileReader();
+  reader.readAsArrayBuffer(selFile);
+
+  // Callback because file reading is async
+  reader.onloadend = function (e) {
+    console.log(e.total);
+  };
+}
